f99f49db92792d587647b6f82fd48ad7901e1ee8
Litvyakov Daniil <140255125+DanyaLitva@users.noreply.github.com>
2026-07-29 19:58:10 +0300

    Update graph_algorithms.h

--- include/graph_algorithms.h
+++ include/graph_algorithms.h
@@ -17,7 +17,7 @@ sparseMtx<int> k_truss(const sparseMtx<int> &A, int k, mspgemmAlgorithm<int> mat
 std::vector<float> brandes_batch(bool isParallel, bool isVectorization, const sparseMtx<int> &A, size_t batch_size);
 
 template <typename T>
-sparseMtx<T> floating_mask_mult(const sparseMtx<T> &A, double mask_density, mspgemmAlgorithm<T> matrixMult, bool isParallel, bool isVectorization);
+sparseMtx<T> floating_mask_mult(const sparseMtx<T> &A, size_t mask_density, mspgemmAlgorithm<T> matrixMult, bool isParallel, bool isVectorization);
 
 
 
@@ -27,19 +27,15 @@ sparseMtx<T> floating_mask_mult(const sparseMtx<T> &A, double mask_density, mspg
 
 //defenition of the template function floating_mask_mult
 template <typename FloatType>
-void floating_mask_mult(const sparseMtx<FloatType> &A, double mask_density, mspgemmAlgorithm<FloatType> matrixMult, bool isParallel, bool isVectorization) {
-    
-    // 1. Строим маску M в CRS формате напрямую под тип FloatType
-    int freq_elements = std::max(1, static_cast<int>(1.0 / mask_density));
+void floating_mask_mult(const sparseMtx<FloatType> &A, size_t mask_density, mspgemmAlgorithm<FloatType> matrixMult, bool isParallel, bool isVectorization) {
     size_t total_elements = static_cast<size_t>(A.m) * A.n;
-    size_t m_nz = (total_elements + freq_elements - 1) / freq_elements;
+    size_t m_nz = (total_elements + mask_density - 1) / mask_density;
     
     sparseMtx<bool> M(A.m, A.n, m_nz);
     std::vector<int> row_counts(A.m, 0);
     
-    // Вычисляем элементы маски
     for (size_t k = 0; k < m_nz; ++k) {
-        size_t global_idx = k * freq_elements;
+        size_t global_idx = k * mask_density;
         size_t row = global_idx / A.n;
         size_t col = global_idx % A.n;
         
@@ -53,12 +49,14 @@ void floating_mask_mult(const sparseMtx<FloatType> &A, double mask_density, mspg
         M.Rst[i+1] = M.Rst[i] + row_counts[i];
     }
 
-    sparseMtx<FloatType> result;
+    sparseMtx<FloatType> result(A.m, A.n);
 
-    // 2. Запуск тестирования
     auto start = std::chrono::steady_clock::now();
+    printf("Count repeat: %zu, Iteration done: ", REPEAT_COUNT_FLOAT);
     for(size_t i = 0; i < REPEAT_COUNT_FLOAT; ++i) {
+        result = sparseMtx<FloatType>(A.m, A.n);
         matrixMult(isParallel, isVectorization, A, A, M, result);
+        printf("%zu ", i + 1);
     }
     auto finish = std::chrono::steady_clock::now();
     
