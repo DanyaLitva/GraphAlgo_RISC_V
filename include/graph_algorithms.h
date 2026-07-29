2bc1936933042bd2cdecca328aa216d986d872b8
Litvyakov Daniil <140255125+DanyaLitva@users.noreply.github.com>
2026-07-29 20:49:41 +0300

    init floatMult

--- include/graph_algorithms.h
+++ include/graph_algorithms.h
@@ -2,8 +2,9 @@
 #include "matrix.h"
 #include <vector>
 #include <chrono>
+#include <iostream>
 
-const size_t REPEAT_COUNT_FLOAT = 10;
+const size_t REPEAT_COUNT_FLOAT = 1;
 
 template <typename T>
 using mspgemmAlgorithm = void(*)(bool, bool, const sparseMtx<T>&, const sparseMtx<T>&, const sparseMtx<T>&, sparseMtx<T>&);
@@ -16,22 +17,12 @@ sparseMtx<int> k_truss(const sparseMtx<int> &A, int k, mspgemmAlgorithm<int> mat
 
 std::vector<float> brandes_batch(bool isParallel, bool isVectorization, const sparseMtx<int> &A, size_t batch_size);
 
-template <typename T>
-sparseMtx<T> floating_mask_mult(const sparseMtx<T> &A, size_t mask_density, mspgemmAlgorithm<T> matrixMult, bool isParallel, bool isVectorization);
-
-
-
-
-
-
-
-//defenition of the template function floating_mask_mult
 template <typename FloatType>
 void floating_mask_mult(const sparseMtx<FloatType> &A, size_t mask_density, mspgemmAlgorithm<FloatType> matrixMult, bool isParallel, bool isVectorization) {
     size_t total_elements = static_cast<size_t>(A.m) * A.n;
     size_t m_nz = (total_elements + mask_density - 1) / mask_density;
     
-    sparseMtx<bool> M(A.m, A.n, m_nz);
+    sparseMtx<FloatType> M(A.m, A.n, m_nz);
     std::vector<int> row_counts(A.m, 0);
     
     for (size_t k = 0; k < m_nz; ++k) {
@@ -41,7 +32,7 @@ void floating_mask_mult(const sparseMtx<FloatType> &A, size_t mask_density, mspg
         
         row_counts[row]++;
         M.Col[k] = col;
-        M.Val[k] = true;
+        M.Val[k] = FloatType(1);
     }
     
     M.Rst[0] = 0;
@@ -52,13 +43,13 @@ void floating_mask_mult(const sparseMtx<FloatType> &A, size_t mask_density, mspg
     sparseMtx<FloatType> result(A.m, A.n);
 
     auto start = std::chrono::steady_clock::now();
-    printf("Count repeat: %zu, Iteration done: ", REPEAT_COUNT_FLOAT);
+    // printf("Count repeat: %zu, Iteration done: ", REPEAT_COUNT_FLOAT);
     for(size_t i = 0; i < REPEAT_COUNT_FLOAT; ++i) {
         result = sparseMtx<FloatType>(A.m, A.n);
         matrixMult(isParallel, isVectorization, A, A, M, result);
-        printf("%zu ", i + 1);
+        // printf("%zu ", i + 1);
     }
     auto finish = std::chrono::steady_clock::now();
     
-    std::cout << "Time:       " << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() << " ms\n";
+    std::cout << "\nTime:       " << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() << " ms\n";
 }
--- src/floatMult.cpp
+++ src/floatMult.cpp
@@ -8,12 +8,109 @@
 #include <algorithm>
 #include <sstream>
 #include <functional>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
+struct GraphInfo {
+    std::string graphName;
+    std::string graphPath;
+    std::string logPath;
+    std::string format;
+};
+
+GraphInfo get_graph_info(int argc, const char *argv[]) {
+    string graphFullPath(argv[1]);
+    string logfile(argv[2]);
+
+    int slashPos = graphFullPath.size() - 1;
+    int dotPos = graphFullPath.size() - 1;
+
+    while (slashPos >= 0 && graphFullPath[slashPos] != '/')
+        --slashPos;
+    while (dotPos >= 0 && graphFullPath[dotPos] != '.')
+        --dotPos;
+
+    string graphName = graphFullPath.substr(slashPos + 1, dotPos - slashPos - 1);
+    string format = graphFullPath.substr(dotPos + 1);
+    string graphPath = graphFullPath.substr(0, slashPos + 1);
+
+    return GraphInfo{graphName, graphPath, logfile, format};
+}
+
+template <typename FloatType>
+int launch_float_test(const sparseMtx<FloatType> &gr, const GraphInfo &info, int argc, const char *argv[]) {
+    string parOrSeq(argv[4]);
+    string vecOrScal(argv[5]);
+    string multiplicationAlgorithm(argv[6]);
+    size_t mask_density = stoull(argv[7]);
+
+    bool isParallel = (parOrSeq == "par");
+    bool useVectorization = (vecOrScal == "vec");
+
+    if (vecOrScal != "vec" && vecOrScal != "scal") {
+        cerr << "incorrect input, 5-th argument: must be 'vec' or 'scal'\n";
+        return -6;
+    }
+    if (parOrSeq != "par" && parOrSeq != "seq") {
+        cerr << "incorrect input, 4-th argument: must be 'par' or 'seq'\n";
+        return -5;
+    }
+
+    mspgemmAlgorithm<FloatType> mxm_algorithm;
+
+    if (multiplicationAlgorithm == "naive")
+        mxm_algorithm = mspgemm_naive<FloatType>;
+    else if (multiplicationAlgorithm == "msa")
+        mxm_algorithm = mspgemm_msa<FloatType>;
+    else if (multiplicationAlgorithm == "mca")
+        mxm_algorithm = mspgemm_mca<FloatType>;
+    else if (multiplicationAlgorithm == "heap")
+        mxm_algorithm = mspgemm_heap<FloatType>;
+    else {
+        cerr << "incorrect input, 6-th argument: has to be 'naive', 'msa', 'mca' or 'heap'\n";
+        return -7;
+    }
+
+    cout << "Graph name: " << info.graphName << '\n';
+    cout << "Vertices:   " << gr.m << '\n';
+    cout << "Edges:      " << gr.nz << '\n';
+    cout << "Type:       " << (typeid(FloatType) == typeid(float) ? "float" : "double") << '\n';
+    cout << "Density:    " << mask_density << '\n';
+    cout << "Algorithm:  " << multiplicationAlgorithm << '\n';
+    cout << "Parallel:   " << (isParallel ? "yes" : "no") << '\n';
+    cout << "Vectorized: " << (useVectorization ? "yes" : "no") << '\n';
+
+    floating_mask_mult<FloatType>(gr, mask_density, mxm_algorithm, isParallel, useVectorization);
+
+    return 0;
+}
+
+template <typename FloatType>
+int read_graph_and_launch_test(const GraphInfo &info, int argc, const char *argv[]) {
+    sparseMtx<FloatType> gr(argv[1], info.format.c_str());
+    cerr << "finished reading\n";
+    return launch_float_test<FloatType>(gr, info, argc, argv);
+}
+
 int main(int argc, const char* argv[]) {
-    printf("Start bench\n");
-    
+    if (argc < 8) {
+        cerr << "Usage: " << argv[0] << " <graph_path> <log_path> <float|double> <par|seq> <vec|scal> <naive|msa|mca|heap> <mask_density>\n";
+        return -1;
+    }
+
+    GraphInfo info = get_graph_info(argc, argv);
+    string valType(argv[3]);
+
+    if (valType == "float") {
+        return read_graph_and_launch_test<float>(info, argc, argv);
+    } else if (valType == "double") {
+        return read_graph_and_launch_test<double>(info, argc, argv);
+    } else {
+        cerr << "unknown value type (expected float or double)\n";
+        return -2;
+    }
 
     return 0;
 }
