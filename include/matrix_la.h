7a97b27d96adba8f116611e3f161856930d73c3a
DanyaLitva <140255125+DanyaLitva@users.noreply.github.com>
2026-08-25 16:56:11 +0300

    Update matrix_la.h

--- include/matrix_la.h
+++ include/matrix_la.h
@@ -766,19 +766,14 @@ void _mspgemm_mca_parallel_scalar(const sparseMtx<T> &A, const sparseMtx<T> &B, 
 template<typename T, typename U>
 void _mspgemm_mca_parallel_vectorized(const sparseMtx<T> &A, const sparseMtx<T> &B, const sparseMtx<U> &M, sparseMtx<T> &C) {
 #ifdef USE_RVV
-  //std::cerr << "Vectorization\n"; 
-
   int mca_len = 0;
-  for (size_t i = 0; i < A.m; ++i) {
-    if (M.Rst[i + 1] - M.Rst[i] > mca_len) {
+  for (size_t i = 0; i < A.m; ++i)
+    if (M.Rst[i + 1] - M.Rst[i] > mca_len)
       mca_len = M.Rst[i + 1] - M.Rst[i];
-    }
-  }
 
 #pragma omp parallel
   {
     MCA<T> accum(mca_len);
-
 #pragma omp for schedule(dynamic)
     for (size_t i = 0; i < A.m; ++i) {
       int m_row_len = M.Rst[i + 1] - M.Rst[i];
@@ -792,7 +787,6 @@ void _mspgemm_mca_parallel_vectorized(const sparseMtx<T> &A, const sparseMtx<T> 
 
         int m_pos = M.Rst[i];
         int m_max = M.Rst[i] + m_row_len;
-
 
         while (b_pos < b_max && m_pos < m_max) {
           int current_b_col = B.Col[b_pos];
@@ -811,42 +805,24 @@ void _mspgemm_mca_parallel_vectorized(const sparseMtx<T> &A, const sparseMtx<T> 
           //m4 version
           // Запрашиваем длину вектора
           size_t vl = __riscv_vsetvl_e32m4(m_max - m_pos);
-
-          // Загружаем вектор индексов
           vint32m4_t v_m_cols = __riscv_vle32_v_i32m4(&M.Col[m_pos], vl);
-
-          // Сравниваем
           vbool8_t v_match = __riscv_vmseq_vx_i32m4_b8(v_m_cols, current_b_col, vl);
-
-          // Ищем индекс первого совпадения
           long match_idx = __riscv_vfirst_m_b8(v_match, vl);
 
           if (match_idx >= 0) {
-            // Вычисляем абсолютное смещение j в accum_ptr
             int j = (m_pos - M.Rst[i]) + match_idx;
             accum_ptr[j] += a_val * B.Val[b_pos];
-
             b_pos++;
-            // Поскольку M.Col строго возрастает, можем перепрыгнуть пройденное
             m_pos += match_idx + 1;
           }
           else {
-            // Совпадений в текущем векторе M нет.
-            // Проверяем последний загруженный элемент вектора M, чтобы понять, 
-            // кто "отстает" и кого нужно двигать дальше.
             int last_m_col = M.Col[m_pos + vl - 1];
-
-            if (current_b_col > last_m_col) {
-              // current_b_col больше всех элементов в векторе, шагаем M вперед на весь вектор (vl)
+            if (current_b_col > last_m_col)
               m_pos += vl;
-            }
-            else {
-              // current_b_col меньше последнего элемента M, значит его вообще нет в этом блоке M. Шагаем B.
+            else
               b_pos++;
-            }
           }
         }
-
       }
 
       memcpy(C.Val + C.Rst[i], accum.values, m_row_len * sizeof(T));
@@ -854,7 +830,7 @@ void _mspgemm_mca_parallel_vectorized(const sparseMtx<T> &A, const sparseMtx<T> 
     }
   }
 #else
-  //std::cerr << "No RVV build\n"; 
+  std::cerr << "No RVV build\n"; 
   _mspgemm_mca_parallel_scalar(A, B, M, C);
 #endif
 }
